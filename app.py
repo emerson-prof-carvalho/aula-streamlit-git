bf3e8b3388ec2cd0e46eb4f6c8c6cc1721cf85a9
Emerson Assis de Carvalho <emerson.carvalho@ifsuldeminas.edu.br>
2025-01-27 23:51:13 +0000

    Versão final da aula.

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import streamlit as st
+import re
 
 st.header("Calculadora", divider="gray")
 
-expression = st.text_input("Entre com a expressão")
+def evaluate_expression(value):
+  if re.match(r'^[0-9+\-*/(). ]+$', value):
+    try:
+      result = eval(value)
+      return result
+    except Exception as e:
+      return f"Erro na expressão: {str(e)}"
+  else:
+    return 'erro'
 
-if (expression):
-  result = eval(expression)
+expression = st.text_input("Entre com a Expressão Matemática")
 
-  st.write("Resultado: ", result)
-  
+if expression:
+  result = evaluate_expression(expression)
+
+  if (result == 'erro'):
+    st.write("Expressão matemática inválida!")
+  else:
+    st.write("Resultado: ", result, ".")
